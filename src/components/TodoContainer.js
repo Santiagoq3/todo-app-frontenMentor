7c53ef434e8c433daea276ca7943a6e837f16032
Santiago Quipildor <Quipildorsantiago@gmail.com>
2022-04-07 17:42:08 -0300

    todo form

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -3,14 +3,52 @@ import "./todocontainer.css"
 export const TodoContainer = () => {
   return (
     <main className='todocontainer'>
-        <div>
+        <div className='todocontainer_container-theme'>
             <h1>T O D O</h1>
+            <button>
+              cambiar theme
+            </button>
         </div>
         <form>
             <div className='todocontainer_container-input'>
-                <input type="text" placeholder='Create a new todo' />
+                <input type="text" placeholder='Create a new todo' autoFocus />
             </div>
         </form>
+        <div className='todocontainer_container-todo'>
+          <ul>
+            <li>
+              <div className='todocontainer_container-todo-title'>
+                <input type="radio" />
+                <p>Learn React</p>
+              </div>
+              <button>
+                delete
+              </button>
+            </li>
+            
+            <li>
+              <div className='todocontainer_container-todo-title'>
+                <input type="radio" />
+                <p>Learn React</p>
+              </div>
+              <button>
+                delete
+              </button>
+            </li>
+          </ul>
+          <div className='todocontainer_container-actions'>
+            <button>
+              All
+            </button>
+            <button>
+              Active
+            </button>
+            <button>
+              Completed
+            </button>
+          </div>
+        </div>
+        
     </main>
   )
 }
